f90443d4a5fb70785af36854457874446280c26e
tycrek <josh.moore@jmoore.dev>
2019-10-17 08:40:43 -0600

    Moved all routes to router.js

--- index.js
+++ index.js
@@ -4,9 +4,6 @@ const PORT = 8235;
 var express = require('express');
 var compress = require('compression');
 var helmet = require('helmet');
-var fs = require('fs-extra');
-var path = require('path');
-var sass = require('node-sass');
 
 // Express app setup
 var app = express();
@@ -15,59 +12,7 @@ app.use(helmet({
 	noCache: true,
 	referrerPolicy: true
 }));
-
-// Preset error response just in case
-app.use((_req, res, next) => {
-	res.type('text');
-	res.status(500);
-	next();
-})
-
-// Express routes
-app.get('/', (_req, res) => {
-	readFile('client/index.html')
-		.then((html) => (res.status(200).type('html'), html))
-		.catch((err) => err)
-		.then((data) => res.send(data));
-});
-
-app.get('/js', (_req, res) => {
-	readFile('client/jmoore.js')
-		.then((js) => (res.status(200).type('js'), js))
-		.catch((err) => err)
-		.then((data) => res.send(data));
-});
-
-app.get('/css', (_req, res) => {
-	readFile('client/stylesheet.scss')
-		.then((scss) => renderSass(scss))
-		.then((css) => (res.status(200).type('css'), css))
-		.catch((err) => err)
-		.then((data) => res.send(data));
-});
+app.use(require('./router'));
 
 // Run Express app
 app.listen(PORT, () => console.log(`Server hosted on port: ${PORT}`));
-
-// Read files with path and buffer conversion
-function readFile(filename) {
-	return new Promise((resolve, reject) => {
-		fs.readFile(path.join(__dirname, filename))
-			.then((bytes) => bytes.toString())
-			.then((data) => resolve(data))
-			.catch((err) => reject(err));
-	});
-}
-
-// Render SASS to CSS
-function renderSass(scss) {
-	return new Promise((resolve, reject) => {
-		sass.render({
-			data: scss,
-			outputStyle: 'compressed'
-		}, (err, result) => {
-			if (err) reject(err);
-			else resolve(result.css.toString());
-		});
-	});
-}
