862d3095911bde9c07ddba305640d081811b5786
tycrek <josh.moore@jmoore.dev>
2019-10-26 20:06:59 -0600

    Improved error handling and cleanup

--- server/router.js
+++ server/router.js
@@ -5,14 +5,19 @@ const sass = require('node-sass');
 const pug = require('pug');
 
 router.get('/', (_req, res) => renderPug(res, 'index'));
-router.get('/css', (_req, res) => renderSass(res));
+router.get('/css', (_req, res, next) => renderSass(res, next));
 router.get(CONFIG.routes, (req, res) => renderPug(res, req.path.split('/')[1]));
 
 // 404 & 500 codes
 router.use((_req, res) => res.status(404).send(CONFIG.http_404));
-router.use((err, _req, res, _next) => (console.error(err.stack), res.status(500).send(CONFIG.http_500)));
+router.use(errorHandler);
 
 module.exports = router;
+
+function errorHandler(err, _req, res, _next) {
+	console.error(err.stack);
+	res.status(500).send(CONFIG.http_500);
+}
 
 function renderPug(res, page) {
 	let title = 'Test'; //TODO: Fix titles
@@ -21,12 +26,7 @@ function renderPug(res, page) {
 	res.render('main', locals);
 }
 
-function renderSass(res) {
-	sass.render({
-		file: CONFIG.path('../client/sass/main.scss'),
-		outputStyle: 'compressed'
-	}, (err, result) => {
-		if (err) throw new err;
-		else res.type('css').send(result.css.toString());
-	});
+function renderSass(res, next) {
+	let options = { file: CONFIG.path('../client/sass/main.scss'), outputStyle: 'compressed' };
+	sass.render(options, (err, result) => err ? next(err) : res.type('css').send(result.css));
 }
