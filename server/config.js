712f8ece91f22aa517e42f64e0359f011285f868
tycrek <josh.moore@jmoore.dev>
2019-10-30 09:27:49 -0600

    Added empty files/routes for about and green

--- server/config.js
+++ server/config.js
@@ -3,9 +3,11 @@ module.exports = {
 	port: 8235,
 	http_404: '<title>404 - Page not found</title>404 - Page not found',
 	http_500: '<title>500 - Internal server error</title>500 - Internal server error',
-	routes: ['/bus'],
+	routes: ['/bus', '/green', '/about'],
 	titles: {
 		index: 'Joshua Moore - Developer',
-		bus: 'Route 401 - MacEwan'
+		bus: 'Route 401 - MacEwan',
+		about: 'About - Joshua Moore',
+		green: 'Environment'
 	}
 };
