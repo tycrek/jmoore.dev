1880dd0983056a031cf38af133a108bf84fee1b3
tycrek <josh.moore@jmoore.dev>
2019-11-01 14:35:16 -0600

    Updated script again

--- update.sh
+++ update.sh
@@ -3,17 +3,17 @@
 echo 'Pulling changes from Git (master)...'
 git pull origin master\
 && echo 'Done'\
-|| echo 'Failed' && exit 1
+|| echo 'Failed'
 
 # Update modules
 echo 'Updating npm packages...'
 npm i\
 && echo 'Done'\
-|| echo 'Failed' && exit 1
+|| echo 'Failed'
 
 echo 'Restarting systemd service...'
 systemctl restart node-jmoore.dev.service\
 && echo 'Done'\
-|| echo 'Failed' && exit 1
+|| echo 'Failed'
 
 echo 'Website has been updated!'; exit 0
