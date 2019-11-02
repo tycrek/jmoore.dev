05a6ca5bbd223e319c461e85a12c09725a1917fc
tycrek <josh.moore@jmoore.dev>
2019-11-02 14:59:52 -0600

    Minor restructure and code cleanup

--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ const app = express();
 app.use(require('compression')());
 app.use(require('helmet')());
 app.use(require('serve-favicon')(CONFIG.path('../client/static/favicon.ico')));
+
 app.use(express.static(CONFIG.path('../client/static')));
 app.use(require('./router'));
 
