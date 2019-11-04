8b5493a5df676b1e573afee6a4da0278299b677c
tycrek <josh.moore@jmoore.dev>
2019-11-04 08:57:54 -0700

    Slightly better 404 and 500 pages

--- server/config.js
+++ server/config.js
@@ -1,8 +1,8 @@
 module.exports = {
 	path: file => require('path').join(__dirname, file),
 	port: 8235,
-	http_404: '<title>404 - Page not found</title>404 - Page not found',
-	http_500: '<title>500 - Internal server error</title>500 - Internal server error',
+	http_404: '<title>404 - Page not found</title><center><h1>404 - Page not found</h1></center>',
+	http_500: '<title>500 - Internal server error</title><center><h1>500 - Internal server error</h1></center>',
 	titles: {
 		index: 'Joshua Moore - Developer',
 		bus: 'Bus Schedules',
