c6412eb1bba8d4ebad09d7f60e20d02f3f51b8df
tycrek <josh.moore@jmoore.dev>
2019-11-06 09:19:22 -0700

    Deleted data.js; major improvements to data access

--- server/utils.js
+++ server/utils.js
@@ -1,11 +1,11 @@
 const path = require('path');
+const fs = require('fs-extra');
 const pino = require('pino')({
 	prettyPrint: process.env.NODE_ENV === 'production' ? false : true
 });
 
 module.exports = {
 	log: pino,
-	path: file => path.join(__dirname, file),
 	CONFIG: {
 		port: 8235,
 		icon: path.join(__dirname, '../client/static/favicon.ico'),
@@ -20,5 +20,15 @@ module.exports = {
 	http: {
 		_404: '<title>404 - Page not found</title><center><br><br><h1>404 - Page not found</h1></center>',
 		_500: '<title>500 - Internal server error</title><center><br><br><h1>500 - Internal server error</h1></center>'
+	},
+	path: file => path.join(__dirname, file),
+	getData: page => {
+		return new Promise((resolve, reject) => {
+			let filepath = path.join(__dirname, page ? `../data/${page}.json` : '../data/main.json');
+			fs.pathExists(filepath)
+				.then(exists => exists ? fs.readJson(filepath) : resolve())
+				.then(json => resolve(json))
+				.catch(err => reject(err));
+		});
 	}
 };
