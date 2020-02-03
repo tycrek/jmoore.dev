7cd0b4537ccc36b7474ceacc0064a727249ad511
tycrek <josh.moore@jmoore.dev>
2020-02-03 13:51:10 -0700

    Remove upload/files; Clean Code conduct updates

--- server/server.js
+++ server/server.js
@@ -3,72 +3,59 @@ const cluster = require('cluster');
 const express = require('express');
 const app = express();
 
+// Only allow paths ending in trailing slash (/) (except for direct file paths)
 app.enable('strict routing');
 
+/*  Compress any eligible traffic;
+    security enhancements;
+    logging for Express functions;
+    modern Favicon serving */
 app.use(require('compression')());
 app.use(require('helmet')());
 app.use(require('express-pino-logger')({ logger: log }));
 app.use(require('serve-favicon')(CONFIG.icon));
-app.use(require('express-fileupload')(CONFIG.upload));
 
+// Add Link header for HTTP/2 Push events
 app.use((_req, res, next) => {
-	let h2_push_urls = [
-		// Static
-		'/css',
-		'/bus.js',
-		'/images/joshua-moore-1.jpg',
-		'/images/jm-vancouver-bc.jpeg',
-		'/images/transit-2019fall_commuter_map_web-1.jpg',
+	let pushHeader = '';
+	CONFIG.h2PushPaths.forEach((path) => pushHeader += (`<${path}>; rel=preload, `));
 
-		// Site pages
-		'/uses/',
-		'/projects/',
-		'/emby/',
-
-		// Bus
-		'/bus/',
-		'/bus/macewan/',
-		'/bus/uofa/',
-		'/bus/nait/',
-		'/bus/nait/413/',
-		'/bus/uofa/414/',
-		'/bus/uofa/404/',
-		'/bus/macewan/401/',
-		'/bus/macewan/411/',
-		'/bus/macewan/413/',
-		'/bus/macewan/411/saturday/',
-		'/bus/macewan/411/sunday/',
-	], header = '';
-	h2_push_urls.forEach((path) => {
-		header += (`<${path}>; rel=preload, `);
-	});
-	res.header('Link', header);
+	// Remove the trailing comma and attach the header to the response
+	res.header('Link', pushHeader.substring(0, pushHeader.length - 1));
 	next();
 });
+
+// Static Express routes (for JavaScript, images, robots.txt, manifests, etc.)
 app.use(express.static(CONFIG.static));
 app.use('/images', express.static(CONFIG.images));
-app.use('/files', [(req, res, next) => req.url.endsWith('/') && req.url !== '/' ? res.redirect(`/files${req.url.substring(0, req.url.length - 1)}`) : next(), express.static(CONFIG.upload.path)]);
-app.use('/upload', require('express-basic-auth')(CONFIG.upload.auth));
+
+// Route handler
 app.use(require('./router'));
 
+// Pug.js renderer
 app.set('views', CONFIG.views);
 app.set('view engine', 'pug');
 
+// Call proper task for multithreading
 cluster.isMaster ? masterThread() : workerThread();
 
+// Thread for master (only runs at launch, possibly after a crash or server reboot)
 function masterThread() {
+	// One thread per CPU
 	let cpus = require('os').cpus().length;
 	log.info(`Forking ${cpus} worker threads`);
 
+	// Create new threads for each CPU core
 	for (let cpu = 0; cpu < cpus; cpu++) cluster.fork();
 
-	cluster.on('online', worker => log.info(`Worker online [${worker.id}]`));
+	cluster.on('online', (worker) => log.info(`Worker online [${worker.id}]`));
 	cluster.on('exit', (worker, code, signal) => {
 		log.warn(`Worker exited, reason: ${signal || code} [${worker.id}]`);
 		cluster.fork();
 	});
 }
 
+// Thread for worker (spawned by master thread at launch or after another thread crashed)
 function workerThread() {
 	app.listen(CONFIG.port, () => log.info(`Server hosted (0.0.0.0:${CONFIG.port}) [${cluster.worker.id}]`));
 }
