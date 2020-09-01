c299c72eeb2494a6dc25082a5ad7adad6399d6d8
tycrek <josh.moore@jmoore.dev>
2020-09-01 07:43:15 -0600

    Added remote update script

---
+++ update-remote.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+echo
+echo 'Running remote update through SSH...'
+
+ssh admin@jmoore.dev "cd jmoore.dev/ && npm run update"
+
+&& echo 'Finished remote update'\
+|| echo 'Failed remote update'
+
